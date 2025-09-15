45dede85aeb689f3243fa729a06b9841b3391145
MicahLi <670706603@qq.com>
2025-09-15 21:49:09 +0800

    添加代码注释,帮助理解代码,已完成分词,encode和decode以及词向量的embedding和位置向量的embedding组合成输入的嵌入向量input_embedding

--- pkg/llms_from_scratch/ch02.py
+++ pkg/llms_from_scratch/ch02.py
@@ -10,18 +10,25 @@ import tiktoken
 
 class GPTDatasetV1(Dataset):
     def __init__(self, txt, tokenizer, max_length, stride):
+        # 使用传入的编码器对给定的文本进行编码
         self.tokenizer = tokenizer
         self.input_ids = []
         self.target_ids = []
 
+        #将给定的文本编译成每一个id对应的列表
         # Tokenize the entire text
         token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
 
+        # 将整个id列表分割成多个长度为max_length的子列表
         # Use a sliding window to chunk the book into overlapping sequences of max_length
         for i in range(0, len(token_ids) - max_length, stride):
+            # 将分割的输入id存入临时的chunk列表中
             input_chunk = token_ids[i:i + max_length]
+            # 将分割的目标id存入临时的chunk列表中
             target_chunk = token_ids[i + 1: i + max_length + 1]
+            # 将分割的临时输入id列表转化为tensor对象添加进输入id列表中
             self.input_ids.append(torch.tensor(input_chunk))
+            # 将分割的临时目标id列表转化为tensor对象添加进目标id列表中
             self.target_ids.append(torch.tensor(target_chunk))
 
     def __len__(self):
@@ -33,12 +40,17 @@ class GPTDatasetV1(Dataset):
 
 def create_dataloader_v1(txt, batch_size=4, max_length=256,
                          stride=128, shuffle=True, drop_last=True, num_workers=0):
+    # 分词器使用gpt2分词器进行分词及转化为对应id列表
     # Initialize the tokenizer
     tokenizer = tiktoken.get_encoding("gpt2")
 
+    # 根据传入文本,分词器及窗口大小及步长创建数据加载器
     # Create dataset
     dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
-
+    # 将创建好的数据集传入数据加载器中,batch_size代表一个批次的样本数量,例如id一共100行,如果batch_size=4,则每次取出4行数据进行训练
+    # shuffle=True代表每次取出的样本都是随机的,False代表每次取出的样本是按顺序的
+    # drop_last=True代表每次取出的样本数量是固定的,False代表每次取出的样本数量是可变的
+    # num_workers=0代表数据加载器使用单线程进行数据加载
     # Create dataloader
     dataloader = DataLoader(
         dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
--- pkg/llms_from_scratch/tests/test_ch02.py
+++ pkg/llms_from_scratch/tests/test_ch02.py
@@ -27,28 +27,38 @@ def test_dataloader(tmp_path, file_name):
 
     vocab_size = 50257
     output_dim = 256
+    # 此处的值书本上写的是4
     context_length = 1024
-
+    # 定义一个256列,50257行的矩阵 token嵌入层,内部装有随机的初始化值,表示初始权重,最终会优化每个列的权重值
     token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+    # 书本上得值代表的是创建一个4行256列的矩阵,此处是1024行256列的矩阵
+    # 定义一个1024行,256列的矩阵 位置嵌入层,默认是有随机的初始值
     pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
 
+    # 每8行数据作为一个批次
     batch_size = 8
+    # 窗口大小是4
     max_length = 4
+    # 根据上述参数构造数据加载器
     dataloader = create_dataloader_v1(
         raw_text,
         batch_size=batch_size,
         max_length=max_length,
         stride=max_length
     )
-
+    # 每个批次遍历处理数据,一个批次是8行数据
     for batch in dataloader:
+        # 此处取出的x和y分别是8行4列的数据,已经过debug验证,没有任何问题
         x, y = batch
-
+        # 将取出的8行4列的x数据从嵌入层中取出这8行4列数据的每一个数据对应的256个数据表示的向量值,也就是一个8行4列256格的三维矩阵
         token_embeddings = token_embedding_layer(x)
+        # torch.arange(x)表示生成一个一位数组,从0到x的一个一维数组,此处的x等于4,表示值0到3的
+        # 此处使用的是1024行256列的矩阵,然后取出矩阵中的0到3行数据用来表示位置嵌入向量
+        # 也就是说此处会取出一个4行256列的矩阵
         pos_embeddings = pos_embedding_layer(torch.arange(max_length))
-
+        # 然后用这个8行4列256格的三维矩阵的矩阵加上4行256列的矩阵,得到一个8行4列8行4列256格的记录了位置向量的三维矩阵
+        # 实际上就是8行的每一行都是一个4行256列的矩阵,然后每个矩阵加上位置嵌入向量,
+        # 也就是4行256列的矩阵,就是同维度向量加法就将位置信息添加到token嵌入向量中了
         input_embeddings = token_embeddings + pos_embeddings
-
         break
-
     input_embeddings.shape == torch.Size([8, 4, 256])
